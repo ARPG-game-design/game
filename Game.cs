2fc4f535a951eae309e243f664f5fe3932199639
sue-qui <80835702+sue-qui@users.noreply.github.com>
2021-03-17 22:59:58 +0800

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Game
+{
+    public static SaveData sav = new SaveData();
+}
+
+public class SaveData
+{
+    public float maxHp = 5f;
+    public float hp = 5.0f;
+
+    public float Damage(float dmg)
+    {
+        hp = Mathf.Clamp(hp - dmg, 0, maxHp);
+        return hp;
+    }
+}
